578119709e1f98354387c555fee25601d9c672db
koshface <koshface@gmail.com>
2022-12-04 20:56:13 -0600

    Updated weather icon

--- src/index.js
+++ src/index.js
@@ -52,8 +52,14 @@ function showTemperature(response) {
   let descriptionElement = document.querySelector("#weather-type");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   descriptionElement.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = `${temperature}`;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
